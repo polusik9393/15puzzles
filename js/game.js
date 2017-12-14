d17bbc549a375aa8eee4be25effd22722655c80c
polusik9393 <polusik9393@mail.ru>
2017-12-14 21:44:30 +0300

    added the main part of game

--- js/game.js
+++ js/game.js
@@ -1,15 +1,79 @@
-Array.prototype.shuffle = function () {
-    let newArr = this.slice(), i = this.length, randIndex;
-    while (i) {
-        randIndex = Math.floor(Math.random() * i);
-      [newArr[i-1], newArr[randIndex]] = [newArr[randIndex], newArr[i-1]];
-      i--;
-    }
-    return newArr;
+var arr = [],
+    box, ei, ej;
+Array.prototype.shuffle = function(i1, j1, i2, j2) {
+    t = this[i1][j1];
+    this[i1][j1] = this[i2][j2];
+    this[i2][j2] = t;
 }
 
-let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+window.onload = function() {
+    box = document.getElementById("box");
+    newGame();
+    document.getElementById("reset").onclick = newGame;
+}
 
-let resArr = arr.shuffle();
+function cellClick(event) {
+    var event = event || window.event,
+        el = event.srcElement || event.target,
+        i = el.id.charAt(0),
+        j = el.id.charAt(2);
+    if ((i == ei && Math.abs(j - ej) == 1) || (j == ej && Math.abs(i - ei) == 1)) {
+        document.getElementById(ei + " " + ej).innerHTML = el.innerHTML;
+        el.innerHTML = "";
+        ei = i;
+        ej = j;
+        var q = true;
+        for (i = 0; i < 4; ++i)
+            for (j = 0; j < 4; ++j)
+                if (i + j != 6 && document.getElementById(i + " " + j).innerHTML != i * 4 + j + 1) {
+                    q = false;
+                    break;
+                }
+        if (q) alert("Victory!");
+    }
+}
 
-item1.innerHTML = arr[0];
+function newGame() {
+    for (i = 0; i < 4; ++i) {
+        arr[i] = []
+        for (j = 0; j < 4; ++j) {
+            if (i + j != 6)
+                arr[i][j] = i * 4 + j + 1;
+            else
+                arr[i][j] = "";
+        }
+    }
+    ei = 3;
+    ej = 3;
+    for (i = 0; i < 1600; ++i)
+        switch (Math.round(3 * Math.random())) {
+            case 0:
+                if (ei != 0) arr.shuffle(ei, ej, --ei, ej);
+                break; // up
+            case 1:
+                if (ej != 3) arr.shuffle(ei, ej, ei, ++ej);
+                break; // right
+            case 2:
+                if (ei != 3) arr.shuffle(ei, ej, ++ei, ej);
+                break; // down
+            case 3:
+                if (ej != 0) arr.shuffle(ei, ej, ei, --ej); // left
+        }
+    var table = document.createElement("table"),
+        tbody = document.createElement("tbody");
+    table.appendChild(tbody);
+    for (i = 0; i < 4; ++i) {
+        var row = document.createElement("tr");
+        for (j = 0; j < 4; ++j) {
+            var cell = document.createElement("td");
+            cell.id = i + " " + j;
+            cell.onclick = cellClick;
+            cell.innerHTML = arr[i][j];
+            row.appendChild(cell);
+        }
+        tbody.appendChild(row);
+    }
+    if (box.childNodes.length == 1)
+        box.removeChild(box.firstChild);
+    box.appendChild(table);
+}
